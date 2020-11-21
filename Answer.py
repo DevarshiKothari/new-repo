474f5b73273e0d19f595990062291c35a87595a1
Devarshi Kothari <47155981+DevarshiKothari@users.noreply.github.com>
2020-11-21 17:26:47 +0530

    Update Answer.py

--- Answer.py
+++ Answer.py
@@ -1,17 +1,13 @@
 from collections import defaultdict
 
-def fun1():
-    return 0
-
 def subs(s):
     n=len(s)
     x=len(set(list(s)))
-    d=defaultdict(fun1)
+    d=defaultdict(lambda :0)
     c=0
     low=0
     high=n
     y=-1
-
     for j in range(n):
         d[s[j]]=d[s[j]]+1
         if d[s[j]]==1:
@@ -21,14 +17,11 @@ def subs(s):
                 if d[s[low]]>1:
                     d[s[low]]=d[s[low]]-1
                 low=low+1
-
             y=j-low+1
             if high>y:
                 high=y
                 z=low
-
     return len(str(s[z:z+high]))
-
 
 s=input()
 print(subs(s))
